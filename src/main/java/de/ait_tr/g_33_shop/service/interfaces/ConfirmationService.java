04d30758c049290e190bc3b0197a2a41c6bf0103
SvintsovAnton <Svintsov.anton94@gmail.com>
2024-06-29 12:12:31 +0200

    lesson17

---
+++ src/main/java/de/ait_tr/g_33_shop/service/interfaces/ConfirmationService.java
@@ -0,0 +1,9 @@
+package de.ait_tr.g_33_shop.service.interfaces;
+
+import de.ait_tr.g_33_shop.domain.entity.User;
+
+public interface ConfirmationService {
+
+    String generateConfirmationCode(User user);
+
+}
